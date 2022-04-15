ae06a793d1642f5bfc1b8de9203089947846e5e0
Sylvere Armange <sylvere.armange@gmail.com>
2022-04-15 18:42:25 +0200

    icon config

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,13 @@ module.exports = {
 		msTileColor: '#000000',
 		appleMobileWebAppCapable: 'yes',
 		appleMobileWebAppStatusBarStyle: 'black',
+		iconPaths: {
+			faviconSVG: null,
+			favicon32: 'img/icons/favicon-32x32.png',
+			favicon16: 'img/icons/favicon-16x16.png',
+			appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+			maskIcon: null,
+			msTileImage: 'img/icons/msapplication-icon-144x144.png'
+		}
 	}
 }
